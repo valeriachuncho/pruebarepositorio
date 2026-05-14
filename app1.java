45e247cd5012d60b0d25151725368a3a94a0fa50
valeriachuncho <vechuncho@utpl.edu.ec>
2026-05-14 08:34:28 -0500

    Add files via upload

---
+++ app1.java
@@ -0,0 +1,22 @@
+public class app1 {
+    public static void main(String[] args) {
+
+        String matriz [][] = new String[3][3];
+        matriz[0][0] = "java";
+        matriz[0][1] = "c++";
+        matriz[0][2] = "java";
+        matriz[1][0] = "python";
+        matriz[1][1] = "java";
+        matriz[1][2] = "rust";
+        matriz[2][0] = "java";
+        matriz[2][1] = "go ";
+        matriz[2][2] = "java";
+
+        String palabra ="java";
+        app1 tarea = new app1();
+
+
+        }
+    }
+
+}
---
+++ tarea.java
@@ -0,0 +1,28 @@
+
+public class tarea extends Thread {
+    private  String fila= [];
+    private  palabra;
+
+    private tarea(String  matriz[][]){
+        this.matriz= matriz;
+    }
+    @Override
+    public String toString() {
+        for ( var matriz1 ::app1){
+            var similar  = true ;
+            for (var  i =0 , i  < 3,i++){
+                for (int j =0 ; j < 3; j++){
+                    if(palabr
+                }
+
+
+            }
+        }
+
+    }
+
+
+
+
+
+}
